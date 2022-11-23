f7f706eed9b27996a172505b847887f5ce35ade6
asdfptj <taejunb70@gmail.com>
2022-11-23 15:11:59 +0900

    screen_display

---
+++ src/main/java/com/example/springbootjsp/controller/BoardController.java
@@ -0,0 +1,21 @@
+package com.example.springbootjsp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class BoardController {
+
+    static final String VIEW_MAIN = "/main"; // 메인화면
+
+    @GetMapping("/")
+    public ModelAndView getBoard(ModelAndView mav) {
+        mav.addObject("value","Hello Spring And gradle");
+
+        mav.setViewName(VIEW_MAIN);
+
+        return mav;
+    }
+}
